0fc30794a915a8e5cd9967338e3028d8489652bd
Алексей П <qudaeo@yamdex.ru>
2020-11-11 21:48:29 +0700

    add dialogs-1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import React from 'react'
 import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from "react-router-dom";
+import {Route, BrowserRouter} from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
@@ -15,9 +16,9 @@ const App = () => {
                 <Header/>
                 <Navbar/>
                 <div className="app-wrapper-content">
+                    <Route exact path='/' component={News} />
                     <Route path='/dialogs' component={Dialogs} />
                     <Route path='/profile' component={Profile} />
-
                     <Route path='/news' component={News} />
                     <Route path='/music' component={Music} />
                     <Route path='/settings' component={Settings} />
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,16 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return(
-        <div>
-            Dialogs
+        <div className={s.Dialogs}>
+            <div className={s.dialogItems}>
+                <div className={s.dialog + ' ' + s.active}>Dimych</div>
+                <div className={s.dialog}>Pavel</div>
+                <div className={s.dialog}>Yana</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.massage}>hey</div>
+                <div className={s.massage}>yo</div>
+            </div>
         </div>
     )
 }

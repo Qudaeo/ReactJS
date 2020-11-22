ddd0c3b955d8f66247bc72d89ba84650f7532807
Qudaeo <Qudaeo@yandex.ru>
2020-11-22 15:09:35 +0700

    createRef

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,19 +1,35 @@
+import React from "react";
 import s from './Dialogs.module.css'
 import DialogItem from "./DialogItem/DialogItem";
 import MessageItem from "./MessageItem/MessageItem";
 
 const Dialogs = (props) => {
+    let newMessageElement = React.createRef();
+
+    let addMessage = () => {
+        alert(newMessageElement.current.value)
+    }
 
     let userElements = props.state.users.map(u => <DialogItem userId={u.id} userName={u.name}/>)
     let messageElements = props.state.messages.map(m => <MessageItem message={m.message} isMyMessage={m.isMyMessage}/>)
 
     return (
-        <div className={s.Dialogs}>
-            <div className={s.dialogItems}>
-                {userElements}
+        <div>
+            <div className={s.Dialogs}>
+                <div className={s.dialogItems}>
+                    {userElements}
+                </div>
+                <div className={s.messages}>
+                    {messageElements}
+                </div>
             </div>
-            <div className={s.messages}>
-                {messageElements}
+            <div className={s.addMessage}>
+                <div>
+                    <textarea ref={newMessageElement}/>
+                </div>
+                <div>
+                    <button onClick={addMessage}>New message</button>
+                </div>
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,22 +1,27 @@
-import s from './MyPosts.module.css'
+import React from "react";
+import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 const MyPosts = (props) => {
+    let newPostElement = React.createRef();
+
     let postElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
+
+    const addPost = () => {
+        alert(newPostElement.current.value)
+    }
 
     return (
         <div className={s.myPosts}>
 
             <h3>my posts</h3>
             <div>
-                <div>
-                    <textarea/>
-                </div>
-                <div>
-                    <button>Add post</button>
-                </div>
-                {postElements}
+                <textarea ref={newPostElement}/>
             </div>
+            <div>
+                <button onClick={addPost}>Add post</button>
+            </div>
+            {postElements}
         </div>
     )
 }

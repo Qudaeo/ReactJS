0e42317a128f5447bbb545c4382b3d36a52bcd1f
Qudaeo <qudaeo@yandex.ru>
2020-12-09 18:30:44 +0700

    add loading.gif in UsersContainer.jsx

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,40 +1,47 @@
-import React from "react";
-import {connect} from "react-redux";
-import {followAC, setCurrentPageAC, setUsersAC, unfollowAC} from "../../redux/usersReducer";
-import axios from "axios";
-import Users from "./Users";
+import React from 'react';
+import {connect} from 'react-redux';
+import {follow, setCurrentPage, setUsers, toggleIsFetching, unfollow} from '../../redux/usersReducer';
+import axios from 'axios';
+import Users from './Users';
+import Preloader from "../common/Preloader/Preloader";
 
 class UsersContainer extends React.Component {
     componentDidMount() {
+        this.props.toggleIsFetching(true)
         axios
             .get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageUsersSize}&page=${this.props.currentPage}`)
             .then(response => {
-
+                this.props.toggleIsFetching(false)
                 this.props.setUsers(response.data.items, response.data.totalCount)
             })
     }
 
-    onChangePage=(pageNumber)=>{
+    onChangePage = (pageNumber) => {
         this.props.setCurrentPage(pageNumber)
+        this.props.toggleIsFetching(true)
         axios
             .get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageUsersSize}&page=${pageNumber}`)
             .then(response => {
-
+                this.props.toggleIsFetching(false)
                 this.props.setUsers(response.data.items, response.data.totalCount)
             })
     }
 
     render() {
-        return <Users
-            users={this.props.users}
-            totalUsersCount={this.props.totalUsersCount}
-            pageUsersSize={this.props.pageUsersSize}
-            currentPage={this.props.currentPage}
+        return <>
+            {this.props.isFetching&& <Preloader /> }
 
-            onChangePage={this.onChangePage}
-            follow={(userId) => this.props.follow(userId)}
-            unfollow={(userId) => this.props.unfollow(userId)}
-        />
+            <Users
+                users={this.props.users}
+                totalUsersCount={this.props.totalUsersCount}
+                pageUsersSize={this.props.pageUsersSize}
+                currentPage={this.props.currentPage}
+
+                onChangePage={this.onChangePage}
+                follow={(userId) => this.props.follow(userId)}
+                unfollow={(userId) => this.props.unfollow(userId)}
+            />
+        </>
     }
 }
 
@@ -42,14 +49,22 @@ let mapStateToProps = (state) => ({
     users: state.usersPage.users,
     currentPage: state.usersPage.currentPage,
     totalUsersCount: state.usersPage.totalUsersCount,
-    pageUsersSize: state.usersPage.pageUsersSize
+    pageUsersSize: state.usersPage.pageUsersSize,
+    isFetching: state.usersPage.isFetching
 })
 
-let mapDispatchToProps = (dispatch) => ({
+/*let mapDispatchToProps = (dispatch) => ({
     follow: (userId) => dispatch(followAC(userId)),
     unfollow: (userId) => dispatch(unfollowAC(userId)),
     setUsers: (users, totalUsersCount) => dispatch(setUsersAC(users, totalUsersCount)),
-    setCurrentPage: (currentPage) => dispatch(setCurrentPageAC(currentPage))
-})
+    setCurrentPage: (currentPage) => dispatch(setCurrentPageAC(currentPage)),
+    toggleIsFetching: (isFetching) => dispatch(toggleIsFetchingAC(isFetching))
 
-export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer)
+})*/
+
+export default connect(mapStateToProps,
+    {
+        follow, unfollow, setUsers,
+        setCurrentPage,toggleIsFetching
+    }
+)(UsersContainer)
--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -1,13 +1,15 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
-const SET_USERS = 'SET-USERS';
-const SET_CURRENT_PAGE = 'SET-CURRENT-PAGE';
+const SET_USERS = 'SET_USERS';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const TOGGLE_IS_FETCHING = 'TOGGLE_IS_FETCHING';
 
 let initialFriendsPage = {
     users: [],
     currentPage: 1,
     totalUsersCount: 0,
-    pageUsersSize: 100
+    pageUsersSize: 100,
+    isFetching: true
 }
 
 const usersReducer = (state = initialFriendsPage, action) => {
@@ -46,15 +48,22 @@ const usersReducer = (state = initialFriendsPage, action) => {
                 ...state,
                 currentPage: action.currentPage
             }
+        case TOGGLE_IS_FETCHING:
+            return {
+                ...state,
+                isFetching: action.isFetching
+            }
 
         default:
             return state
     }
 }
 
-export const followAC = (userId) => ({type: FOLLOW, userId});
-export const unfollowAC = (userId) => ({type: UNFOLLOW, userId});
-export const setUsersAC = (users, totalUsersCount) => ({type: SET_USERS, users, totalUsersCount});
-export const setCurrentPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage});
+export const follow = (userId) => ({type: FOLLOW, userId});
+export const unfollow = (userId) => ({type: UNFOLLOW, userId});
+export const setUsers = (users, totalUsersCount) => ({type: SET_USERS, users, totalUsersCount});
+export const setCurrentPage = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage});
+export const toggleIsFetching = (isFetching) => ({type: TOGGLE_IS_FETCHING, isFetching});
+
 
 export default usersReducer
